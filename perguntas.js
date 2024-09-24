3f5c7e85eb44a9c29d451a6e47f363cddfef9ba4
Eduardokorb2023 <148217028+Eduardokorb2023@users.noreply.github.com>
2024-09-24 19:05:21 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,3 @@
+criaCartao('Programação', 'O que é JavaScript?', 'JavaScript é uma linguagem de programação.');
+criaCartao('Geografia', 'Qual a capital da França?', 'A capital da França é Paris.');
+criaCartao('Língua Inglesa', 'Como se diz "Oi" em inglês?', 'Oi em inglês se diz "Hi".');
