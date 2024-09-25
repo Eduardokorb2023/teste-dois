5b52581c51dfb85c07dc201dfc82f183ee2b57b1
Eduardokorb2023 <148217028+Eduardokorb2023@users.noreply.github.com>
2024-09-25 18:49:00 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
 function criaCartao(categoria, pergunta, resposta) {
-    let container = document.getElementById('container');
-    let cartao = document.createElement('article');
-    cartao.className = 'cartao';
+    let container = document.getElementById('container')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
+
     cartao.innerHTML = `
-        <div class="cartao__conteudo">
-            <h3>${categoria}</h3>
-            <div class="cartao_conteudo_pergunta">
-                <p>${pergunta}</p>
-            </div>
-            <div class="cartao_conteudo_resposta">
-                <p>${resposta}</p>
-            </div>
-        </div>
-    `;
+          <div class="cartao__conteudo">
+                                <h3>${categoria}</h3>
+                                <div class="cartao__conteudo__pergunta">
+                                        <p>${pergunta}</p>
+                                </div>
+                                <div class="cartao__conteudo__resposta">
+                                       <p>${resposta}</p>
+                                </div>
+                        </div>
+    `
     cartao.addEventListener('click', function () {
-        cartao.classList.toggle('active');
-    });
-    container.appendChild(cartao);
+        cartao.classList.toggle('active')
+    })
+
+let respostaestavisivel = false
+
+function viracartao() {
+    respostaestavisivel = !respostaestavisivel
+    cartao.classList.toggle('active',respostaestavisivel)
+}
+
+    container.appendChild(cartao)
 }
--- perguntas.js
+++ perguntas.js
@@ -1,3 +1,30 @@
-criaCartao('Programação', 'O que é JavaScript?', 'JavaScript é uma linguagem de programação.');
-criaCartao('Geografia', 'Qual a capital da França?', 'A capital da França é Paris.');
-criaCartao('Língua Inglesa', 'Como se diz "Oi" em inglês?', 'Oi em inglês se diz "Hi".');
+criaCartao('desconhecido',
+     ' como sonegar imposto', 
+    'se você olho provavelmente você que se preso.'
+);
+criaCartao(
+    'ciências', 
+    'quantos dias podemos ficar sem água?', 
+    'cerca de dois a quatro dias.'
+);
+criaCartao(
+    'espanhol',
+    'Como se diz "JACARÉ" em espanhol?',
+    'caimán'
+    )
+
+    criaCartao('charada',
+        'sobe e desce, mas nunca se mexe', 
+       'a escada.'
+   );
+   criaCartao(
+       'Geografia', 
+       'qual o maior país do mundo?', 
+       'rússia.'
+   );
+   criaCartao(
+       'música',
+       'Como música começa com: amogo faz o pix...',
+       'o nome da musica é amigo faz o pix, e você pode olha ela no final do site '
+       
+       )
